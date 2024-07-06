a6e89ba31dbc782afe8abdfe66963e28169ae56b
Ashishmehta786 <135453659+Ashishmehta786@users.noreply.github.com>
2024-07-06 17:53:10 +0530

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<div className="text-xl">
+  hello 
+</div>
+    </>
+  )
+}
+
+export default App
