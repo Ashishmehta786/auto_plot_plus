349ab630ed49186a1dda9cfff73b69f754d09a4a
Ashishmehta786 <135453659+Ashishmehta786@users.noreply.github.com>
2024-07-08 22:40:57 +0530

    delete frontend

--- backend/backend_django/account/models.py
+++ backend/backend_django/account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Accountcreate(models.Model):
+    name=models.CharField(max_length=10)
+    email=models.EmailField()
+    phone=models.IntegerField()
+
+
+
+    
